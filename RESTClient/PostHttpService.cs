b1e972d120fc555a7ca6f15a595fc01319e72e15
Caticu <radumarian250797@gmail.com>
2022-04-25 07:53:35 +0200

    assignment 2

---
+++ RESTClient/PostHttpService.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+
+namespace RESTClient;
+
+public class PostHttpService : IPostService
+{
+    public Task<ICollection<Post>> GetPostAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> GetPostByID(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> AddPost(Post post)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ RESTClient/UserHttpService.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Domain.Contracts;
+using Domain.Models;
+
+namespace RESTClient;
+
+public class UserHttpService : IUserService
+{
+    public Task<ICollection<User>> GetUsersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserByID(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUser(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> AddUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteUser(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
